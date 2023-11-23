a4447b217dd361b294ca84047477eef16f47f81c
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2023-11-24 02:52:25 +0600

    Outlet container added for max witdth - xl

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../Shared/NavBar";
+import { Container } from "@mui/material";
 
 const MainLayout = () => {
   return (
     <div>
       <NavBar />
-      <Outlet />
+      <Container maxWidth="xl">
+        <Outlet />
+      </Container>
     </div>
   );
 };
