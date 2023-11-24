b251e4e1b4899f6dc042b48a82eb2d704dbe562a
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2023-11-25 02:06:22 +0600

    post count data fetched succesfully

--- src/Pages/Home/AllPosts.jsx
+++ src/Pages/Home/AllPosts.jsx
@@ -1,27 +1,31 @@
-import React from 'react';
-import useAxiosPublic from '../../Hooks/useAxiosPublic';
-import { useQuery } from '@tanstack/react-query';
+import React from "react";
+import useAxiosPublic from "../../Hooks/useAxiosPublic";
+import { useQuery } from "@tanstack/react-query";
 
 const AllPosts = () => {
+  const axiosPublic = useAxiosPublic();
+  const {
+    data: postsData = [],
+    isLoading: isPostDataLoading,
+    refetch: postRefetch,
+  } = useQuery({
+    queryKey: ["posts"],
+    queryFn: async () => {
+      const res = await axiosPublic.get("/posts");
+      return res.data;
+    },
+  });
 
-    const axiosPublic = useAxiosPublic();
-    const {
-      data: postsData=[],
-      isLoading: isPostDataLoading,
-      refetch: postRefetch,
-    } = useQuery({
-      queryKey: ["posts"],
-      queryFn: async () => {
-        const res = await axiosPublic.get("/posts");
-        return res.data;
-      },
-    });
+  const { data: postCountData = {} } = useQuery({
+    queryKey: ["postsCount"],
+    queryFn: async () => {
+      const res = await axiosPublic.get("/posts-count");
+      return res.data;
+    },
+  });
+  console.log(postCountData.postsCount);
 
-    return (
-        <div>
-            total posts: {postsData.length }
-        </div>
-    );
+  return <div>total posts: {postsData.length}</div>;
 };
 
 export default AllPosts;
