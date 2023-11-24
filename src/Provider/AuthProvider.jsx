d13cb785d3a9db86efd04e6f9fc543ecd6118d5a
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2023-11-24 15:39:26 +0600

    authprovider sign in and sign out and google sign in and observer setup done

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,20 +1,75 @@
-import { createContext } from "react";
-import { GoogleAuthProvider, getAuth } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import { app } from "../FireBase/Firebase.config";
-
 
 // authcontext
 export const AuthContext = createContext(null);
 // auth for firebase
 const auth = getAuth(app);
 // provider for google
-const googeleProvider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({ children }) => {
-  const demoUser = { name: "nahid" };
+  //use state for user
+  const [user, setUser] = useState(null);
+  // use state for user Loading time
+  const [loading, setLoading] = useState(true);
+
+  // google sign in
+  const googleLogIn = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  // singup with email pasword
+  const emailPassSignup = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  // Email Passwornd login
+  const emailPassLogin = (email, password) => {
+    setLoading(true);
+
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  // signOut
+  const signoutUser = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  // User observer
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return () => {
+      unSubscribe();
+    };
+  }, []);
+
+  // const demoUser = { name: "nahid" };
 
   const authValues = {
-    demoUser,
+    // demoUser,
+    googleLogIn,
+    signoutUser,
+    user,
+    loading,
+    emailPassSignup,
+    emailPassLogin,
   };
 
   return (
