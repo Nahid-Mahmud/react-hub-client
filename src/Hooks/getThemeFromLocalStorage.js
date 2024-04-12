bc7b46d064a370f3fd3db06b01443929844198f3
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2024-04-12 17:16:45 +0600

    dark mode default added for initial render of first page visit

--- src/Hooks/getThemeFromLocalStorage.js
+++ src/Hooks/getThemeFromLocalStorage.js
@@ -1,6 +1,5 @@
 // get theme data from local storage
 
 export const getThemeFromLocalStoragae = () => {
-    return localStorage.getItem("_theme") || "light"; // if _theme item not found it will return "light" as default value
-  };
-  
+  return localStorage.getItem("_theme") || "dark"; // if _theme item not found it will return "light" as default value
+};
--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -41,7 +41,7 @@ const MainLayout = () => {
   ) : (
     <div className="dark:bg-[#0f172a] dark:text-slate-300">
       <NavBar />
-      <div className="min-h-[calc(100vh-170.5px)] mt-[66px]">
+      <div className="min-h-[calc(100vh-170.5px)] mt-16">
         {navigation.state === "loading" ? ( // If loading or navigation is in progress, display the preloader
           <Loader />
         ) : (
